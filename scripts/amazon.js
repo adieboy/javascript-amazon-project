20575bab76d1d4c927d016325782bc2e748c2da3
Adie <capilitanadie@gmail.com>
2023-08-02 04:19:25 +0800

    LATEST

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from './utils/money.js';
 let productsHTML = '';
@@ -64,13 +64,13 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 const addedMessageTimeouts = {};
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
+  const cartQuantity = calculateCartQuantity();
 
-  cart.forEach( (cartItem) =>{
-    cartQuantity += cartItem.quantity;
-  });
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
+
+updateCartQuantity();
+
 
 function addedMessageNotif(addedMessage,productId){
   addedMessage.classList.add('added-to-cart-visible');
